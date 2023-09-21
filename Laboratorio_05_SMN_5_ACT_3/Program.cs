59f6f3cac92b6e3dda9001abf94b26de8d4ffa26
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-09-20 23:15:22 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_05_SMN_5_ACT_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Crear un programa que permita ingresar n notas y hallar el promedio de estas.
+double promedio;
+double Resultado = 0;
+Console.Write("Ingrese el numero de notas: ");
+if (!int.TryParse(Console.ReadLine(), out int n)
+    || n <= 0);
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"Ingrese la nota {i}: ");
+    if (!int.TryParse(Console.ReadLine(), out int notas) || notas < 0 || notas > 20)
+    return; Resultado += notas;
+}
+ promedio = Resultado / n;
+Console.WriteLine($"El promedio es {promedio}" );
